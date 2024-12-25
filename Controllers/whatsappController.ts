ddf4a59d7e9704601c07a45fd781aaac405ef6a2
JeronimoNP <familiadojeronimo@gmail.com>
2024-12-24 21:03:52 -0300

    Atualizado, com as seguintes novas featch, sem login, logar pelo terminal pelo qr code

--- Controllers/whatsappController.ts
+++ Controllers/whatsappController.ts
@@ -2,31 +2,51 @@
 // import makeWASocket, { DisconnectReason, useMultiFileAuthState, WASocket } from '@whiskeysockets/baileys';
 import response from '../Controllers/whatssapResponse';
 
+
 // let sock: WASocket | null = null;
 
 import makeWASocket, { AnyMessageContent, delay, fetchLatestBaileysVersion } from '@whiskeysockets/baileys'
 import { useMultiFileAuthState } from '@whiskeysockets/baileys'
 import Pino from 'pino'
-
 // Configurações de autenticação
 
 const logger = Pino({ level: 'info' })
 
 async function startSock() {
     try {
+        
         // Obtém a versão mais recente do WhatsApp Web
         const { version, isLatest } = await fetchLatestBaileysVersion()
         console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
         const { state, saveCreds } = await useMultiFileAuthState('./auth_info_baileys')
-        // Cria uma nova instância do socket do WhatsApp
-        const sock = makeWASocket({
-            version, // Define a versão do WhatsApp Web
-            logger, // Usa o logger definido anteriormente
-            auth: state // Usa o estado de autenticação carregado do arquivo
-        })
-        response.escuta(sock, state)
-        // Atualiza as credenciais de autenticação sempre que houver mudanças
-        sock.ev.on('creds.update', saveCreds)
+
+        // Verifica se há arquivos de autenticação se não autenticar
+        const fs = require('fs')
+        if(fs.readdirSync('../bot.whatssap/auth_info_baileys').length){
+            // Cria uma nova instância do socket do WhatsApp com autenticação qr-code
+            console.log("Não há arquivos de autenticação, Fazendo autenticação")
+            const sock = makeWASocket({
+                version, // Define a versão do WhatsApp Web
+                printQRInTerminal: true, // Usa o logger definido anteriormente
+                auth: state // Usa o estado de autenticação carregado do arquivo
+            });
+            // Atualiza as credenciais de autenticação sempre que houver mudanças
+            sock.ev.on('creds.update', saveCreds);
+            //escuta as conversar e responde
+            response.escuta(sock, state)
+        }else{
+
+            // Cria uma nova instância do socket do WhatsApp com autenticação ultimo estado
+            const sock = makeWASocket({
+                version, // Define a versão do WhatsApp Web
+                logger, // Usa o logger definido anteriormente
+                auth: state // Usa o estado de autenticação carregado do arquivo
+            })
+            // Atualiza as credenciais de autenticação sempre que houver mudanças
+            sock.ev.on('creds.update', saveCreds)
+            //escuta as conversar e responde
+            response.escuta(sock, state)
+        }
 
 
         console.log("Conectado ao WhatsApp")
